0a38f13f76ba19ef377ef8d3001ba03c09565b47
kano1701 <thunderbird2711@gmail.com>
2019-05-04 01:00:34 +0500

    Рендер страницы, добавлен таймер

---
+++ timer.js
@@ -0,0 +1,37 @@
+var counterMin = -1;
+
+function Min()
+{
+	counterMin++;
+	document.getElementById("min").innerHTML = counterMin;
+	setTimeout("Min()", 60000);
+}
+
+var counterSec = 0;
+
+function Sec()
+{
+	counterSec++;
+	document.getElementById("sec").innerHTML = counterSec;
+	setTimeout("Sec()", 1000);
+}
+
+var counterMilliSec = 0;
+
+function milliSec()
+{	
+	if(counterMilliSec > 999)
+		counterMilliSec = 20;
+	else
+		counterMilliSec+=10;
+
+	document.getElementById("millisec").innerHTML = counterMilliSec;
+	setTimeout("milliSec()", 7);
+}
+
+function timer()
+{
+	Min();
+	Sec();
+	milliSec();
+}
