1c250f5577f59c8490ec84b159ed9dd481b281c4
kano1701 <thunderbird2711@gmail.com>
2019-05-07 14:50:39 +0500

    Исправил ошибки при откате при не правильном выборе пользователя

--- searchTwoCells.js
+++ searchTwoCells.js
@@ -1,34 +1,42 @@
 var found = [];
 var prePoint = 'White';
-var countPoint = 0;
 
-function pairSearch(id, arrColor){
+function pairSearch(id, arrColor, countPoint){
 	console.log("pairSearch");
 	console.log("arrColor[id] "+arrColor[id]);
+	console.log("found "+found);
+	console.log("countPoint "+countPoint);
+
+	var point = document.getElementById(id);
+	point.style.background = arrColor[id];
+
 
 	if(countPoint % 2 == 0 || countPoint == 0){
 		console.log("countPoint % 2 == 0 || countPoint == 0");
 		prePoint = arrColor[id];
-		point = document.getElementById(id);
-		point.style.background = prePoint;
+		found.push(id);
 		countPoint++;
 	}
 	else if(countPoint % 2 != 0){
 		console.log("countPoint % 2 != 0");
 		if(prePoint == arrColor[id]){
 			console.log("prePoint == arrColor[id]");
-			point = document.getElementById(id);
-			point.style.background = prePoint;
 			found.push(id);
 			countPoint++;
 		}
 		else if(prePoint != arrColor[id]){
 			console.log("prePoint != arrColor[id]");
-			countPoint == 0;
-			for(var i=0;i<found.length;i++){
-				point = document.getElementById(found[i]);
-				point.style.background = 'white';
-			}
+			setTimeout(miss, 500);
 		}	
+	}
+	return countPoint;
+}
+
+function miss(){
+	countPoint = 0;
+	found = [];
+	for(var i=0; i<16; i++){
+		point = document.getElementById(i);
+		point.style.background = 'white';
 	}
 }
--- timer.js
+++ timer.js
@@ -1,40 +1,55 @@
 var counterMin = -1;
 
-function Min()
-{
-	counterMin++;
-	document.getElementById("min").innerHTML = counterMin;
-	setTimeout("Min()", 59000);
+function Min(status){
+	if(status == "start"){
+		counterMin++;
+		document.getElementById("min").innerHTML = counterMin;
+		setTimeout("Min()", 59000);
+	}
+	else if(status == "stop")
+		return document.getElementById("min");
 }
 
 var counterSec = 0;
 
-function Sec()
-{
-	if(counterSec >= 59)
-		counterSec = 0;
-	else
-		counterSec++;
-	document.getElementById("sec").innerHTML = counterSec;
-	setTimeout("Sec()", 1000);
+function Sec(status){
+	if(status == "start"){
+		if(counterSec >= 59)
+			counterSec = 0;
+		else
+			counterSec++;
+		document.getElementById("sec").innerHTML = counterSec;
+		setTimeout("Sec()", 1000);
+	}
+	else if(status == "stop")
+		return document.getElementById("sec");
 }
 
 var counterMilliSec = 0;
 
-function milliSec()
-{	
-	if(counterMilliSec > 999)
-		counterMilliSec = 20;
-	else
-		counterMilliSec+=9;
-
-	document.getElementById("millisec").innerHTML = counterMilliSec;
-	setTimeout("milliSec()", 7);
+function MilliSec(status){
+	if(status == "start"){
+		if(counterMilliSec > 999)
+			counterMilliSec = 20;
+		else
+			counterMilliSec+=9;
+		document.getElementById("millisec").innerHTML = counterMilliSec;
+		setTimeout("MilliSec()", 7);
+	}
+	else if(status == "stop")
+		return document.getElementById("millisec");
 }
 
-function timer()
-{
-	Min();
-	Sec();
-	milliSec();
+function timer(status){	
+	if(status == "start"){
+		Min(status);
+		Sec(status);
+		MilliSec(status);
+	}
+	else if(status == "stop"){
+		var min = Min(status);
+		var sec = Sec(status);
+		var msec = MilliSec(status);
+		return min+":"+sec+":"+msec;
+	}
 }
